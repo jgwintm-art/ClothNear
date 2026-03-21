8c0b11f366c076fa635bcfe44d6ddb4c1a164b4f
jan <jg.wintm@gmail.com>
2026-03-21 22:15:47 +0800

    feat: created store model

---
+++ lib/models/store_model.dart
@@ -0,0 +1,46 @@
+class StoreModel {
+  final String storeId;
+  final String ownerUid;
+  final String storeName;
+  final String location;
+  final String contact;
+  final String description;
+  final String imageUrl;
+  final bool isActive;
+
+  StoreModel({
+    required this.storeId,
+    required this.ownerUid,
+    required this.storeName,
+    required this.location,
+    required this.contact,
+    required this.description,
+    this.imageUrl = '',
+    this.isActive = true,
+  });
+
+  factory StoreModel.fromMap(Map<String, dynamic> map, String id) {
+    return StoreModel(
+      storeId: id,
+      ownerUid: map['ownerUid'] ?? '',
+      storeName: map['storeName'] ?? '',
+      location: map['location'] ?? '',
+      contact: map['contact'] ?? '',
+      description: map['description'] ?? '',
+      imageUrl: map['imageUrl'] ?? '',
+      isActive: map['isActive'] ?? true,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'ownerUid': ownerUid,
+      'storeName': storeName,
+      'location': location,
+      'contact': contact,
+      'description': description,
+      'imageUrl': imageUrl,
+      'isActive': isActive,
+    };
+  }
+}
